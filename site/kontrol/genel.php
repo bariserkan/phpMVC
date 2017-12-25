c2d1760dec13334776b4f9f4f08e99cd3e48934c
bariserkan <bariserkan1994@gmail.com>
2017-12-25 20:46:14 +0300

    v1.1

--- site/kontrol/genel.php
+++ site/kontrol/genel.php
@@ -5,12 +5,16 @@ class genel extends kontrol
 	public function hataBolum()
 	{
 		$veri['title'] = '404 Hata';
+		$veri['baslik'] = '404 Hata!';
+		$veri['icerik'] = $_GET['url'].' sayfası bulunamadı';
 		return $this->tasarla('hata', $veri);
 	}
 
 	public function bakimBolum()
 	{
 		$veri['title'] = 'Bakımda';
+		$veri['baslik'] = 'Bakımda!';
+		$veri['icerik'] = 'Bu Web Sitesi Bakım Dolayısıyla Kapalıdır!';
 		return $this->tasarla('bakim', $veri);
 	}
 }
--- site/tasarim/hata.php
+++ site/tasarim/hata.php
@@ -1,5 +1,28 @@
-<?php require TDIR.'/yapi/header.php' ?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	<title><?php echo $title ?></title>
+	<link rel="stylesheet" href="http://fontawesome.io/assets/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://getbootstrap.com/docs/4.0/examples/cover/cover.css">
+  </head>
+  <body>
+    <div class="site-wrapper">
+      <div class="site-wrapper-inner">
+        <div class="cover-container"> 
+          <main role="main" class="inner cover">
+          <h1 class="cover-heading"><?php echo $baslik ?></h1>
+            <p class="lead"><?php echo $icerik ?>
+          </main>
+          <footer class="mastfoot">
+            <div class="inner">
+              <p><a href="<?php echo URL ?>"><b><?php echo SITE_ADI ?></b></a></p>
+            </div>
+          </footer>
+        </div>
 
-<p>404 HATA<br>
-<?php echo($_GET["url"]." sayfası bulunamadı"); ?></p>
-<?php require TDIR.'/yapi/footer.php' ?>
+      </div>
+
+    </div>
+  </body>
+</html>
